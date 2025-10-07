3fc956bcbda4d27c7d466558dd22325cbfb01a32
MPfria02 <marcel020568@gmail.com>
2025-10-07 17:20:43 -0400

    feat(auth): enhance routing with login and register components, and configure animations
    - Added `LoginComponent` and `RegisterComponent` to the routing configuration.
    - Updated `appConfig` to include `provideHttpClient` with interceptors and `provideAnimations`.
    - Introduced `auth` module exports for better component management.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent, RegisterComponent } from './auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, title:'Login'},
+  { path: 'register', component: RegisterComponent, title: 'Register'},
+  { path: '**', redirectTo: '/login' }
+];
